805fac51163e77965e9d2a3584e5abb717914c0b
daewook.laptop <daeuky1@gmail.com>
2016-03-04 22:58:47 +0900

    초난감 UserDao

---
+++ src/springbook/user/dao/UserDao.java
@@ -0,0 +1,73 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import springbook.user.domain.User;
+
+public class UserDao {
+	public void add (User user) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");		
+		Connection c = DriverManager.getConnection(
+				"jdbc:mysql://localhost/mysql_test","root","root");
+		
+		PreparedStatement ps = c.prepareStatement(
+				"insert into users(id, name, password) values(?, ?, ?)" );
+		
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+		
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+	}
+	
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection(
+				"jdbc:mysql://localhost/mysql_test","root","root");
+		
+		PreparedStatement ps = c.prepareStatement(
+				"select * from users where id = ?");
+		
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+				
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return user;
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		UserDao dao = new UserDao();
+		
+		User user = new User();
+		user.setId("singun");
+		user.setName("Daewook Shin");
+		user.setPassword("123456");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + " register success");
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		
+		System.out.println(user2.getId() + " search success");
+	}
+}
