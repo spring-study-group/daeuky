c6d366903dd54bddd770633a80e600af68b587b6
daewook.laptop <daeuky1@gmail.com>
2016-03-05 00:08:10 +0900

    인터페이스를 통한 느스한 결합 생성

---
+++ src/springbook/user/dao/DConnectionMaker.java
@@ -0,0 +1,18 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DConnectionMaker implements ConnectionMaker {
+
+	@Override
+	public Connection makeConnection() throws ClassNotFoundException,
+			SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		return DriverManager.getConnection(
+				"jdbc:mysql://localhost/mysql_test","root","root");
+	}
+
+}
--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -1,7 +1,6 @@
 package springbook.user.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,14 +8,14 @@ import java.sql.SQLException;
 import springbook.user.domain.User;
 
 public class UserDao {
-	private SimpleConnectionMaker simpleConnectionMaker;
+	private ConnectionMaker connectionMaker;
 	
 	public UserDao() {
-		simpleConnectionMaker = new SimpleConnectionMaker();
+		connectionMaker = new DConnectionMaker();
 	}
 	
 	public void add (User user) throws ClassNotFoundException, SQLException {
-		Connection c = simpleConnectionMaker.getConnection();
+		Connection c = connectionMaker.makeConnection();
 		
 		PreparedStatement ps = c.prepareStatement(
 				"insert into users(id, name, password) values(?, ?, ?)" );
@@ -32,7 +31,7 @@ public class UserDao {
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		Connection c = simpleConnectionMaker.getConnection();
+		Connection c = connectionMaker.makeConnection();
 		
 		PreparedStatement ps = c.prepareStatement(
 				"select * from users where id = ?");
