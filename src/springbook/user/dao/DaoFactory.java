55ff75de014745aae5a13890d868e597c4759657
daewook.laptop <daeuky1@gmail.com>
2016-03-05 01:26:22 +0900

    ConnectionMaker의 관심 분리

--- src/springbook/user/dao/DaoFactory.java
+++ src/springbook/user/dao/DaoFactory.java
@@ -1,9 +1,21 @@
 package springbook.user.dao;
 
 public class DaoFactory {
+//	DAO가 늘어날때 마다 ConnectionMaker 구현 클래스의 오브젝트를 생성하는 코드가 메소드마다 반복
+//	어떤 ConnectionMaker 구현 클래스를 사용하지 결정하는 기능이 중복
+//	public AccountDao accountDao() {
+//		return new AccountDao(new DConnectionMaker());
+//	}
+//	
+//	public MessageDao messageDao() {
+//		return new MessageDao(new DConnectionMaker());
+//	}
+	
 	public UserDao userDao() {
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
-		return userDao;
+		return new UserDao(connectionMaker());
+	}
+	
+	public ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
 	}
 }
