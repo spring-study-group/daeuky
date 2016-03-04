d9b89b81fe02ba5e1fcc2610eaeb5e0aaebea720
daewook.laptop <daeuky1@gmail.com>
2016-03-05 00:12:54 +0900

    클라이언트 생성을 통해 connection에 대한 책임을 위임

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -10,8 +10,8 @@ import springbook.user.domain.User;
 public class UserDao {
 	private ConnectionMaker connectionMaker;
 	
-	public UserDao() {
-		connectionMaker = new DConnectionMaker();
+	public UserDao(ConnectionMaker connectionMaker) {
+		this.connectionMaker = connectionMaker;
 	}
 	
 	public void add (User user) throws ClassNotFoundException, SQLException {
@@ -51,24 +51,5 @@ public class UserDao {
 		c.close();
 		
 		return user;
-	}
-	
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao dao = new UserDao();
-		
-		User user = new User();
-		user.setId("singun");
-		user.setName("Daewook Shin");
-		user.setPassword("123456");
-		
-		dao.add(user);
-		
-		System.out.println(user.getId() + " register success");
-		
-		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		System.out.println(user2.getPassword());
-		
-		System.out.println(user2.getId() + " search success");
 	}
 }
---
+++ src/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,27 @@
+package springbook.user.dao;
+
+import java.sql.SQLException;
+
+import springbook.user.domain.User;
+
+public class UserDaoTest {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		ConnectionMaker connectionMaker = new DConnectionMaker();
+		UserDao dao = new UserDao(connectionMaker);
+		
+		User user = new User();
+		user.setId("singun");
+		user.setName("Daewook Shin");
+		user.setPassword("123456");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + " register success");
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		
+		System.out.println(user2.getId() + " search success");
+	}
+}
