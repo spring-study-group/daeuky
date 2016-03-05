f827f5d39e983c3f6f00f08d77eada1b320ee260
daewook.laptop <daeuky1@gmail.com>
2016-03-05 14:17:36 +0900

    IoC 적용

--- src/springbook/user/dao/DaoFactory.java
+++ src/springbook/user/dao/DaoFactory.java
@@ -1,6 +1,11 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+	
 //	DAO가 늘어날때 마다 ConnectionMaker 구현 클래스의 오브젝트를 생성하는 코드가 메소드마다 반복
 //	어떤 ConnectionMaker 구현 클래스를 사용하지 결정하는 기능이 중복
 //	public AccountDao accountDao() {
@@ -11,10 +16,12 @@ public class DaoFactory {
 //		return new MessageDao(new DConnectionMaker());
 //	}
 	
+	@Bean
 	public UserDao userDao() {
 		return new UserDao(connectionMaker());
 	}
 	
+	@Bean
 	public ConnectionMaker connectionMaker() {
 		return new DConnectionMaker();
 	}
