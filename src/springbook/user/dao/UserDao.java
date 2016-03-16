4fd90c04325cad80d70f80c5d3bf520be2f95df1
daewook.laptop <daeuky1@gmail.com>
2016-03-16 23:03:39 +0900

    nested class

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -20,6 +20,26 @@ public class UserDao {
 	}
 	
 	public void add (User user) throws ClassNotFoundException, SQLException {
+		class AddStatement implements StatementStrategy {
+			private User user;
+			
+			public AddStatement(User user) {
+				this.user = user;
+			}
+
+			public PreparedStatement makePreparedStatement(Connection c)
+					throws SQLException {
+				PreparedStatement ps = 
+						c.prepareStatement("insert into users(id, name, password)" +
+								"values(?,?,?);");
+				ps.setString(1, user.getId());
+				ps.setString(2, user.getName());
+				ps.setString(3, user.getPassword());
+				
+				return ps;
+			}
+		}
+		
 		StatementStrategy st = new AddStatement(user);
 		jdbcContextWithStatementStarategy(st);
 	}
