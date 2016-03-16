cd473d7a1f7278520170c4fc450eac60cb2c671e
daewook.laptop <daeuky1@gmail.com>
2016-03-17 00:06:12 +0900

    DI를 위해 JdbcContext를 분리

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -14,13 +14,18 @@ import springbook.user.domain.User;
 
 public class UserDao {
 	private DataSource dataSource;
+	private JdbcContext jdbcContext;
 	
 	public void setDataSource (DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
 	
+	public void setJdbcContext (JdbcContext jdbcContext) {
+		this.jdbcContext = jdbcContext;
+	}
+	
 	public void add (final User user) throws ClassNotFoundException, SQLException {
-		jdbcContextWithStatementStarategy(new StatementStrategy() {
+		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
 			public PreparedStatement makePreparedStatement(Connection c)
 					throws SQLException {
 				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password)" +
@@ -63,8 +68,7 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-//		StatementStrategy st = new DeleteAllStatement();
-		jdbcContextWithStatementStarategy(new StatementStrategy() {
+		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
 			public PreparedStatement makePreparedStatement(Connection c)
 					throws SQLException {
 				return c.prepareStatement("delete from users");
@@ -105,19 +109,4 @@ public class UserDao {
 		}
 	}
 	
-	public void jdbcContextWithStatementStarategy(StatementStrategy stmt) throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		
-		try {
-			c = dataSource.getConnection();
-			ps = stmt.makePreparedStatement(c);
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (ps != null) { try { ps.close(); } catch (SQLException e) {} }
-			if (c != null) { try { c.close(); } catch (SQLException e) {} }
-		}
-	}
 }
