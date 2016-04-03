8b0f42580305bd0eae53492c1ac0363133fe4abe
daewook.laptop <daeuky1@gmail.com>
2016-04-03 21:37:47 +0900

    refactoring test code - 2

--- src/springbook/user/service/UserServiceTest.java
+++ src/springbook/user/service/UserServiceTest.java
@@ -3,6 +3,9 @@ package springbook.user.service;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+import static springbook.user.service.UserService.MIN_LOGCOUNT_FOR_SILVER;
+import static springbook.user.service.UserService.MIN_RECOMMEND_FOR_GOLD;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -32,10 +35,10 @@ public class UserServiceTest {
 	@Before
 	public void setup() {
 		users = Arrays.asList(
-				new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0),
-				new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0),
-				new User("erwins", "신승한", "p3", Level.SILVER, 60, 29),
-				new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30),
+				new User("bumjin", "박범진", "p1", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER-1, 0),
+				new User("joytouch", "강명성", "p2", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER, 0),
+				new User("erwins", "신승한", "p3", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD-1),
+				new User("madnite1", "이상호", "p4", Level.SILVER, 60, MIN_RECOMMEND_FOR_GOLD),
 				new User("green", "오민규", "p5", Level.GOLD, 100, 100)
 				);
 	}
