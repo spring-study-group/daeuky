44432d1d1d3f1c00a574c34578b56f52f902d309
daewook.laptop <daeuky1@gmail.com>
2016-04-03 20:31:11 +0900

    refactoring some methods

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -17,18 +17,33 @@ public class UserService {
 		List<User> users = userDao.getAll();
 		
 		for (User user : users) {
-			Boolean changed = false;
-			
-			if(user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
-				user.setLevel(Level.SILVER);
-				changed = true;
-			} else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
-				user.setLevel(Level.GOLD);
-				changed = true;
-			} 
-			
-			if(changed)
-				userDao.update(user);
+			if(canUpgradeLevel(user))
+				upgradeLevel(user);
 		}
+	}
+	
+	private boolean canUpgradeLevel(User user) {
+		Level currentLevel = user.getLevel();
+		switch(currentLevel) {
+		case BASIC:
+			return (user.getLogin() >= 50);
+		case SILVER:
+			return (user.getRecommend() >= 30);
+		case GOLD:
+			return false;
+			default: throw new IllegalArgumentException("Unknown Level: " +
+			currentLevel);
+		}
+	}
+	
+	private boolean upgradeLevel(User user) {
+		if (user.getLevel() == Level.BASIC) user.setLevel(Level.SILVER)
+		else if (user.getLevel() == Level.SILVER) user.setLevel(Level.GOLD)
+		userDao.update(user);
+	}
+
+	public void add(User user) {
+		if (user.getLevel() == null) user.setLevel(Level.BASIC);
+		userDao.add(user);
 	}
 }
